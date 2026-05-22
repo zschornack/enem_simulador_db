2fb7fe50666f4f5826d6387d4427358b246efb8a
Miguel Zschornack <120066884+zschornack@users.noreply.github.com>
2026-05-21 23:56:50 -0300

    Add files via upload

--- triggers.sql
+++ triggers.sql
@@ -3,26 +3,36 @@ create extension if not exists pgcrypto;
 create or replace function create_profile()
 returns trigger as $$
 begin
-    insert into profiles (id, auth_users_id, full_name, user_role, created_at)
-    values (gen_random_uuid(), new.id, 'novo usuário', 'student', now());
+
+    insert into profiles (auth_user_id, full_name, user_role, created_at)
+    values (new.id, 'novo usuário', 'student', now());
+    
     return new;
 end;
-$$ language plpgsql;
+$$ language plpgsql security definer; -- tem que contornar o problema que deu antes
 
 create trigger trg_create_profile
-after insert on auth_users
+after insert on auth_user
 for each row
 execute function create_profile();
 
+
 create or replace function validate_school_admin()
 returns trigger as $$
+declare
+    v_role text;
 begin
-    if (select user_role from profiles where id = new.admin_id) <> 'school_admin' then
-        raise exception 'o admin da instituição deve ter user_role = school_admin.';
+    -- var aqui pq antes tava uma merda na moral nao dava pra ler nada
+    select user_role into v_role from profiles where id = new.admin_id;
+
+    -- v_role ser nulo perfil não encontrado ou algo bizarro
+    if coalesce(v_role, '') <> 'school_admin' then
+        raise exception 'O admin da instituição deve ter user_role = school_admin.';
     end if;
+    
     return new;
 end;
-$$ language plpgsql;
+$$ language plpgsql security definer;
 
 create trigger trg_validate_school_admin
 before insert or update of admin_id on institutions
